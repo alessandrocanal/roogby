af116666b6724be81c241dcc3083eeed65872781
Ale <Canal@iMac-di-Canal.local>
2013-08-15 19:12:13 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roogby::Application.config.secret_token = '9dce9ccd2e97458d306b09d3a7c7683f64b19e2c39f3e7237815c9835a1fa3b95e6ab27723b7c36e0f000ed616d5d9b16d3db3a3905f8470c87f52bff4a01a19'
