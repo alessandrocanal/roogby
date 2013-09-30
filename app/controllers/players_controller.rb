9d8b5e24dfd6349426df88d02831a609cf6b5f4e
Ale <Canal@iMac-di-Canal.local>
2013-09-30 21:17:46 +0200

    layout and players stats

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -8,19 +8,24 @@ class PlayersController < ApplicationController
     #Rails.logger.debug("MATCHES----------->#{@matches.to_yaml}")
 
     m = Match.new
+    cpm = CompetitionsPlayersMetric.new
     cpsm = CompetitionsPlayersSeasonsMetric.new
 
     @div_matches = "Matches played"
     @div_competition_stats = "Competitions stats"
+    @div_stats_rank = "Statistics rank by season"
 
     if !session[:season_id].blank?
       @div_matches = "Matches played in "+session[:season_id]
       @div_competition_stats = "Competitions stats in "+session[:season_id]
+      @div_stats_rank = "Statistics rank in "+session[:season_id]
       @matches = m.player_matches(params[:id], session[:season_id])
+      @stats_rank = cpm.player_stats_rank(params[:id], session[:season_id])
       @stats = cpsm.player_stats(params[:id], session[:season_id])
     else
       @matches = m.player_matches(params[:id], "all")
       @stats = cpsm.player_stats(params[:id], "all")
+      @stats_rank = cpm.player_stats_rank(params[:id], "all")
     end
 
     render :layout => 'player'
--- app/models/competitions_players_metric.rb
+++ app/models/competitions_players_metric.rb
@@ -14,4 +14,45 @@ class CompetitionsPlayersMetric < ActiveRecord::Base
       response
     #end
   end
+
+  def player_stats_rank(player_id, season_id)
+    #Rails.cache.fetch("player_stats_rank_"+player_id.to_s+"_"+season_id.to_s, :expires_in => 7.days) do
+    response = Hash.new
+    if season_id != "all"
+      stats = CompetitionsPlayersMetric.includes(:metric, :competition).where("player_id=? AND season_id=?", player_id, season_id).order("competition_id DESC, metric_id ASC")
+    else
+      stats = CompetitionsPlayersMetric.includes(:metric, :competition).where("player_id=?", player_id).order("season_id DESC, competition_id DESC, metrics.kind ASC,metric_id ASC")
+    end
+    stats.each do |s|
+      #Rails.logger.debug("RESP--------->#{s.to_yaml}")
+      if response[s.season_id].blank?
+        response[s.season_id] = Hash.new
+        #Rails.logger.debug("Comp------>#{s.competition.name}")
+        competition = Hash.new
+        competition[s.competition.name] = Array.new
+        metric = Hash.new
+        metric[s.metric.metric] = s.quantity
+        metric["rank"] = s.rank
+        competition[s.competition.name] << metric
+        response[s.season_id] = competition
+
+      elsif response[s.season_id][s.competition.name].blank?
+        response[s.season_id][s.competition.name] = Array.new
+        metric = Hash.new
+        metric[s.metric.metric] = s.quantity
+        metric["rank"] = s.rank
+
+        response[s.season_id][s.competition.name] << metric
+      else
+        metric = Hash.new
+        metric[s.metric.metric] = s.quantity
+        metric["rank"] = s.rank
+        response[s.season_id][s.competition.name] << metric
+
+      end
+    end
+    response
+    #end
+
+  end
 end
